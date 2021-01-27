028042ebccd5c28975bb277dcac2fd2ab12d2e46
jalvarezbarciela <56076790+jalvarezbarciela@users.noreply.github.com>
2021-01-27 17:37:13 +0100

    Scripts Acabados

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;
-
+    private int count;
     private float movementX;
     private float movementY;
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void OnMove(InputValue movementValue)
@@ -26,11 +30,41 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            if (count > 0)
+            {
+                count--;
+                SetCountText();
+            }
+            other.gameObject.transform.position = new Vector3(9, 1, 9);
+            gameObject.transform.position = new Vector3(0, 1, 0);
+        }
+
+    }
+
 
 }
